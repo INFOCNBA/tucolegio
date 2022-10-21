ce78058407540a03faf8c02f35c843cf29e2b712
Informática CNBA <105891917+INFOCNBA@users.noreply.github.com>
2022-10-21 12:42:29 -0300

    Actualizar main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,91 @@
+tick=100 #ms
+Vmax=127
+mismoSentido=False
+
 def on_forever():
-    robotbit.motor_run(robotbit.Motors.M1B, 255)
-    basic.show_icon(IconNames.NO)
-    basic.pause(1000)
-    robotbit.motor_run(robotbit.Motors.M1B, -255)
-    basic.show_icon(IconNames.YES)
-    basic.pause(1000)
+    if input.button_is_pressed(Button.A):
+        mover_motores(Button.A,mismoSentido)
+        reposo()
+    elif input.button_is_pressed(Button.B):
+        mover_motores(Button.B,mismoSentido)
+        reposo()
+    elif input.logo_is_pressed():
+        cambiarModo()        
+
+
+def reposo():
+    #basic.show_icon(IconNames.DIAMOND)
+    robotbit.motor_run(robotbit.Motors.M1A, 0)
+    robotbit.motor_run(robotbit.Motors.M2B, 0)
+    mostrarModo()
+
+def mover_motores(boton,mismoSentido):
+    if boton==Button.A:
+        if mismoSentido:
+            basic.show_arrow(ArrowNames.SOUTH)
+            robotbit.motor_run(robotbit.Motors.M1A, -Vmax)            
+            robotbit.motor_run(robotbit.Motors.M2B, -Vmax)
+        else:
+            basic.show_arrow(ArrowNames.NORTH_WEST)
+            robotbit.motor_run(robotbit.Motors.M1A, Vmax)
+            robotbit.motor_run(robotbit.Motors.M2B, -Vmax)
+
+    elif boton==Button.B:
+        if mismoSentido:
+            basic.show_arrow(ArrowNames.NORTH)
+            robotbit.motor_run(robotbit.Motors.M1A, Vmax)
+            robotbit.motor_run(robotbit.Motors.M2B, Vmax)
+        else:
+            basic.show_arrow(ArrowNames.NORTH_EAST)
+            robotbit.motor_run(robotbit.Motors.M1A, -Vmax)
+            robotbit.motor_run(robotbit.Motors.M2B, Vmax)
+
+    else:
+        print("botonAB no implementado")
+        basic.show_icon(IconNames.ANGRY)
+
+    while input.button_is_pressed(boton):
+        basic.pause(tick)
+
+def mostrarModo():
+    if mismoSentido:
+            basic.show_leds("""
+            . . # . .
+            . # # # .
+            . . . . .
+            . # # # .
+            . . # . .
+            """)
+    else:
+            basic.show_leds("""
+            . . . . .
+            . # . # .
+            # # . # #
+            . # . # .
+            . . . . .
+            """)
+
+
+
+def cambiarModo():
+    global mismoSentido
+    mismoSentido=not(mismoSentido)
+    mostrarModo()
+
+
+def motor1_atras():
+    basic.show_icon(IconNames.NO)    
+    robotbit.motor_run(robotbit.Motors.M1A, -Vmax)
+    while input.button_is_pressed(Button.B):
+        basic.pause(tick)
+
+def on_button_pressed_a():
+    pass
+
+def on_button_pressed_b():
+    pass
+
+#input.on_button_pressed(Button.A, on_button_pressed_a)
+#input.on_button_pressed(Button.B, on_button_pressed_b)
+cambiarModo()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,102 @@
-basic.forever(function on_forever() {
-    robotbit.MotorRun(robotbit.Motors.M1B, 255)
+let tick = 100
+// ms
+let Vmax = 127
+let mismoSentido = false
+function reposo() {
+    // basic.show_icon(IconNames.DIAMOND)
+    robotbit.MotorRun(robotbit.Motors.M1A, 0)
+    robotbit.MotorRun(robotbit.Motors.M2B, 0)
+    mostrarModo()
+}
+
+function mover_motores(boton: number, mismoSentido: boolean) {
+    if (boton == Button.A) {
+        if (mismoSentido) {
+            basic.showArrow(ArrowNames.South)
+            robotbit.MotorRun(robotbit.Motors.M1A, -Vmax)
+            robotbit.MotorRun(robotbit.Motors.M2B, -Vmax)
+        } else {
+            basic.showArrow(ArrowNames.NorthWest)
+            robotbit.MotorRun(robotbit.Motors.M1A, Vmax)
+            robotbit.MotorRun(robotbit.Motors.M2B, -Vmax)
+        }
+        
+    } else if (boton == Button.B) {
+        if (mismoSentido) {
+            basic.showArrow(ArrowNames.North)
+            robotbit.MotorRun(robotbit.Motors.M1A, Vmax)
+            robotbit.MotorRun(robotbit.Motors.M2B, Vmax)
+        } else {
+            basic.showArrow(ArrowNames.NorthEast)
+            robotbit.MotorRun(robotbit.Motors.M1A, -Vmax)
+            robotbit.MotorRun(robotbit.Motors.M2B, Vmax)
+        }
+        
+    } else {
+        console.log("botonAB no implementado")
+        basic.showIcon(IconNames.Angry)
+    }
+    
+    while (input.buttonIsPressed(boton)) {
+        basic.pause(tick)
+    }
+}
+
+function mostrarModo() {
+    if (mismoSentido) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . . . .
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            # # . # #
+            . # . # .
+            . . . . .
+            `)
+    }
+    
+}
+
+function cambiarModo() {
+    
+    mismoSentido = !mismoSentido
+    mostrarModo()
+}
+
+function motor1_atras() {
     basic.showIcon(IconNames.No)
-    basic.pause(1000)
-    robotbit.MotorRun(robotbit.Motors.M1B, -255)
-    basic.showIcon(IconNames.Yes)
-    basic.pause(1000)
+    robotbit.MotorRun(robotbit.Motors.M1A, -Vmax)
+    while (input.buttonIsPressed(Button.B)) {
+        basic.pause(tick)
+    }
+}
+
+function on_button_pressed_a() {
+    
+}
+
+function on_button_pressed_b() {
+    
+}
+
+// input.on_button_pressed(Button.A, on_button_pressed_a)
+// input.on_button_pressed(Button.B, on_button_pressed_b)
+cambiarModo()
+basic.forever(function on_forever() {
+    if (input.buttonIsPressed(Button.A)) {
+        mover_motores(Button.A, mismoSentido)
+        reposo()
+    } else if (input.buttonIsPressed(Button.B)) {
+        mover_motores(Button.B, mismoSentido)
+        reposo()
+    } else if (input.logoIsPressed()) {
+        cambiarModo()
+    }
+    
 })
